b36da3a9db1af93c0eea1b85ecd5acc3cdfd7551
Thomas Castiglione <castiglione@mac.com>
2021-09-14 20:10:54 +0800

    use Skia to generate centered text at marked locations

---
+++ Thousand.CLI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Thousand.CLI
+{
+    class Program
+    {
+        private static readonly string graph = @"
+    node ""Entity One""
+    node ""Entity Two""
+
+    node ""Two-Lined
+Entity""
+
+    node ""Three
+Lined
+Entity""
+";
+
+        static void Main()
+        {
+            var tokenizer = Tokenizer.Build();
+            var document = Parsers.Document(tokenizer.Tokenize(graph));
+            var diagram = Composer.Compose(document.Value);
+            var image = Renderer.Render(diagram);
+
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "thousand-test.png");
+
+            using (var output = File.OpenWrite(path))
+            {
+                image.Encode().SaveTo(output);
+            }
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = path,
+                UseShellExecute = true
+            });
+        }
+    }
+}
---
+++ Thousand.Core/Renderer.cs
@@ -0,0 +1,37 @@
+﻿using SkiaSharp;
+using System;
+
+namespace Thousand
+{
+    public static class Renderer
+    {
+        public static SKImage Render(Layout.Diagram diagram)
+        {
+            var info = new SKImageInfo(diagram.Width, diagram.Height);
+            using var surface = SKSurface.Create(info);            
+            var canvas = surface.Canvas;
+
+            var textBrush = new SKPaint { Color = SKColors.Red, TextAlign = SKTextAlign.Center, TextSize = 20 };
+            var blueFill = new SKPaint { Color = SKColors.Blue };
+            
+            canvas.Clear(SKColors.White);
+            foreach (var label in diagram.Labels)
+            {
+                var textBounds = new SKRect();
+                textBrush.MeasureText("M", ref textBounds);
+
+                canvas.DrawRect(new SKRect(label.X - 50, 40, label.X + 50, 60), blueFill);
+
+                var lines = label.Content.Split(Environment.NewLine);
+                var offset = textBounds.Height / 2 - (lines.Length - 1) * (textBrush.TextSize / 2);
+                foreach (var line in lines)
+                {
+                    canvas.DrawText(line, new SKPoint(label.X, label.Y + offset), textBrush);
+                    offset += textBrush.TextSize;
+                }                
+            }
+
+            return surface.Snapshot();
+        }
+    }
+}
