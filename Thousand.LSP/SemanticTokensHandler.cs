4e5ea8363362d83bc4234ba520bcd78ca311e90a
Thomas Castiglione <castiglione@mac.com>
2021-10-20 19:56:16 +0800

    use the semantic analysis to improve token highlighting

--- Thousand.LSP/SemanticTokensHandler.cs
+++ Thousand.LSP/SemanticTokensHandler.cs
@@ -35,6 +35,12 @@ namespace Thousand.LSP
         protected override async Task Tokenize(SemanticTokensBuilder builder, ITextDocumentIdentifierParams identifier, CancellationToken cancellationToken)
         {
             var document = await semanticService.GetAnalysisAsync(identifier.TextDocument.Uri);
+
+            foreach (var (cref, cdef) in document.ClassReferences)
+            {
+                builder.Push(cref.AsRange(), 0, 0);
+            }
+
             if (document.Syntax != null)
             {
                 foreach (var topLevelClass in document.Syntax.Declarations.Where(d => d.Value.IsT2).Select(d => d.Value.AsT2))
@@ -56,13 +62,6 @@ namespace Thousand.LSP
 
         private static void HighlightClass(SemanticTokensBuilder builder, AST.TolerantClass klass)
         {
-            builder.Push(klass.Name.Span.AsRange(), 0, 0);
-
-            foreach (var call in klass.BaseClasses)
-            {
-                builder.Push(call.Span().AsRange(), 0, 0);
-            }
-
             foreach (var attribute in klass.Attributes)
             {
                 foreach (var token in attribute.Value.Sequence())
@@ -92,11 +91,6 @@ namespace Thousand.LSP
 
         private static void HighlightObject(SemanticTokensBuilder builder, AST.TolerantObject objekt)
         {
-            foreach (var call in objekt.Classes)
-            {
-                builder.Push(call.Span().AsRange(), 0, 0);
-            }
-
             foreach (var attribute in objekt.Attributes)
             {
                 foreach (var token in attribute.Sequence().SkipWhile(t => t.Kind != Parse.TokenKind.EqualsSign).Skip(1))
@@ -126,11 +120,6 @@ namespace Thousand.LSP
 
         private static void HighlightLine(SemanticTokensBuilder builder, AST.TolerantLine line)
         {
-            foreach (var call in line.Classes)
-            {
-                builder.Push(call.Span().AsRange(), 0, 0);
-            }
-
             foreach (var attribute in line.Attributes)
             {
                 foreach (var token in attribute.Sequence().SkipWhile(t => t.Kind != Parse.TokenKind.EqualsSign).Skip(1))
