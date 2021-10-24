f2b45634e43ede12c4d92fda58dff3076f31eec7
Thomas Castiglione <castiglione@mac.com>
2021-10-24 18:03:30 +0800

    docs for all stroke attributes

--- Thousand.Core/Parse/Attributes/StrokeAttributes.cs
+++ Thousand.Core/Parse/Attributes/StrokeAttributes.cs
@@ -9,11 +9,33 @@ namespace Thousand.Parse.Attributes
     {
         public static IEnumerable<Definition> All()
         {
-            yield return Definition.Create("stroke-colour", "stroke-color", Value.Colour, value => new AST.StrokeColourAttribute(value));
-            yield return Definition.Create("stroke-width", Value.Width, value => new AST.StrokeWidthAttribute(value));
-            yield return Definition.Create("stroke-style", Identifier.Enum<StrokeKind>(), value => new AST.StrokeStyleAttribute(value));
+            yield return Definition.Create("stroke-colour", "stroke-color", Value.Colour, value => new AST.StrokeColourAttribute(value), Format.Doc(
+                "Controls the colour of lines. Supports HTML-style colours or a few well-known names.",
+                "`#rrggbb` or `#rgb` (colour)",
+                UseKind.Entity
+            ));
+
+            yield return Definition.Create("stroke-width", Value.Width, value => new AST.StrokeWidthAttribute(value), Format.Doc(
+                "Controls the presence and width of lines. Hairlines are always 1 pixel wide; lines with a set width scale with the document.",
+                "`X` (number), `hairline` or `none`",
+                UseKind.Entity
+            ));
+
+            yield return Definition.Create("stroke-style", Identifier.Enum<StrokeKind>(), value => new AST.StrokeStyleAttribute(value), Format.Doc(
+                "Controls the pattern used to draw lines.",
+                Format.Names<StrokeKind>(),
+                UseKind.Entity
+            ));
+
             yield return Definition.Create("stroke", Attribute.Shorthand(Value.Colour, Value.Width, Identifier.Enum<StrokeKind>()), 
-                                                     values => values switch { (var c, var w, var s) => new AST.StrokeShorthandAttribute(c, s, w) });
+                                                     values => values switch { (var c, var w, var s) => new AST.StrokeShorthandAttribute(c, s, w) }, Format.Doc(
+                "Shorthand for `stroke-colour`, `stroke-width` and `stroke-style`",
+                "any of colour, width and/or style",
+                UseKind.Entity,
+                "stroke=hairline",
+                "stroke=blue 3",
+                "stroke=dashed #00f"
+            ));
         }
     }
 }
