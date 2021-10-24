05df7e14ac0f8923989d2e31fbe33bf5735a80ff
Thomas Castiglione <castiglione@mac.com>
2021-10-24 18:00:10 +0800

    partial docs for all node attributes

---
+++ Thousand.Core/Parse/Attributes/Format.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Thousand.Parse.Attributes
+{
+    internal static class Format
+    {
+        public static string Doc(string? description, string type, UseKind kind, params string[] examples)
+        {
+            var builder = new StringBuilder();
+
+            if (description != null)
+            {
+                builder.Append($@"{description}
+
+");
+            }
+
+            builder.Append(@$"_Value_: {type}
+
+_Applies to:_ {kind switch
+            {
+                UseKind.Object => "objects",
+                UseKind.Line => "lines",
+                UseKind.Document => "the whole diagram",
+                UseKind.Region => "objects or the whole diagram",
+                UseKind.Entity => "objects or lines"
+            }}
+
+_Examples:_ {string.Join(", ", examples.Select(e => $"`{e}`"))}");
+
+            return builder.ToString();
+        }
+
+        public static string Doc(string type, UseKind kind, params string[] examples) => Doc(null, type, kind, examples);
+
+        public static string Names<T>() where T : struct, Enum
+        {
+            return string.Join(", ", Enum.GetNames<T>().Select(n => $"`{n.ToLowerInvariant()}`"));
+        }
+
+        public static string NamesOrNone<T>() where T : struct, Enum
+        {
+            return string.Join(", ", Enum.GetNames<T>().Select(n => $"`{n.ToLowerInvariant()}`")) + " or `none`";
+        }
+    }
+}
--- Thousand.Core/Parse/Attributes/NodeAttributes.cs
+++ Thousand.Core/Parse/Attributes/NodeAttributes.cs
@@ -18,7 +18,7 @@ namespace Thousand.Parse.Attributes
         public static IEnumerable<Definition> All()
         {
             // controls for the intrinsic content 
-            yield return Definition.Create("min-width", Value.CountingDecimal.OrNone(), value => new AST.NodeMinWidthAttribute(value), API.Doc(
+            yield return Definition.Create("min-width", Value.CountingDecimal.OrNone(), value => new AST.NodeMinWidthAttribute(value), Format.Doc(
                 "Increases an object's width to at least `X` pixels. If its content is wider than `X-padding`, it will grow larger.",
                 "`X` (decimal) or `none`",
                 UseKind.Object,
@@ -26,7 +26,7 @@ namespace Thousand.Parse.Attributes
                 "min-width=none"
             ));
 
-            yield return Definition.Create("min-height", Value.CountingDecimal.OrNone(), value => new AST.NodeMinHeightAttribute(value), API.Doc(
+            yield return Definition.Create("min-height", Value.CountingDecimal.OrNone(), value => new AST.NodeMinHeightAttribute(value), Format.Doc(
                 "Increases an object's height to at least `Y` pixels. If its content is taller than `X-padding`, it will grow larger.",
                 "`Y` (decimal) or `none`",
                 UseKind.Object,
@@ -34,14 +34,14 @@ namespace Thousand.Parse.Attributes
                 "min-height=none"
             ));
 
-            yield return Definition.Create("shape", Identifier.Enum<ShapeKind>().OrNone(), value => new AST.NodeShapeAttribute(value), API.Doc(
-                string.Join(", ", System.Enum.GetNames<ShapeKind>().Select(n => $"`{n.ToLowerInvariant()}`")) + " or `none` (in which case the object is just a container)",
+            yield return Definition.Create("shape", Identifier.Enum<ShapeKind>().OrNone(), value => new AST.NodeShapeAttribute(value), Format.Doc(
+                Format.NamesOrNone<ShapeKind>() + " (in which case the object is just a container)",
                 UseKind.Object,
                 "shape=pill",
                 "shape=none"
             ));
 
-            yield return Definition.Create("corner-radius", "corner", Value.WholeNumber, value => new AST.NodeCornerRadiusAttribute(value), API.Doc(
+            yield return Definition.Create("corner-radius", "corner", Value.WholeNumber, value => new AST.NodeCornerRadiusAttribute(value), Format.Doc(
                 "For an object with rounded corners, sets the width and height of the corners to `X`.",
                 "`X` (integer)",
                 UseKind.Object,
@@ -49,24 +49,72 @@ namespace Thousand.Parse.Attributes
                 "corner=0"
             ));
 
-            yield return Definition.Create("label-content", Value.Text, value => new AST.NodeLabelContentAttribute(value));
-            yield return Definition.Create("label-justify", Attribute.AlignColumn, value => new AST.NodeLabelJustifyAttribute(value));
+            yield return Definition.Create("label-content", Value.Text, value => new AST.NodeLabelContentAttribute(value), Format.Doc(
+                "For an object with rounded corners, sets the width and height of the corners to `X`.",
+                "`\"My Object\"` or `MyObject` (string), or `none`",
+                UseKind.Object,
+                "label=\"This is used instead of the object's name.\""
+            ));
+
+            yield return Definition.Create("label-justify", Attribute.AlignColumn, value => new AST.NodeLabelJustifyAttribute(value), Format.Doc(
+                "Positions the label within the object.",
+                "`start`/`left`, `center` or `end`/`right`",
+                UseKind.Object
+            ));
+
             yield return Definition.Create("label", Attribute.Shorthand(Identifier.Enum<AlignmentKind>(), Value.Text), 
-                                                    values => values switch { (var a, var t) => new AST.NodeLabelAttribute(t, a) });
+                                                    values => values switch { (var a, var t) => new AST.NodeLabelAttribute(t, a) }, Format.Doc(
+                "Shorthand for `label-content` and `label-justify`.",
+                "text and/or justification",
+                UseKind.Entity,
+                "label=\"My Object\"",
+                "label=center",
+                "label=boringobject end"
+            ));
 
             // different methods of placing the node in its region - see also "anchor" from EntityAttributes
-            yield return Definition.Create("column", "col", Value.CountingNumber, value => new AST.NodeColumnAttribute(value));
-            yield return Definition.Create("row", Value.CountingNumber, value => new AST.NodeRowAttribute(value));       
+            yield return Definition.Create("column", "col", Value.CountingNumber, value => new AST.NodeColumnAttribute(value), Format.Doc(
+                "`X` (integer)",
+                UseKind.Object
+            ));
+
+            yield return Definition.Create("row", Value.CountingNumber, value => new AST.NodeRowAttribute(value), Format.Doc(
+                "`X` (integer)",
+                UseKind.Object
+            ));       
             
-            yield return Definition.Create("position", "pos", from x in Value.WholeNumber from y in Value.WholeNumber select new Point(x, y), value => new AST.NodePositionAttribute(value));
+            yield return Definition.Create("position", "pos", from x in Value.WholeNumber from y in Value.WholeNumber select new Point(x, y), value => new AST.NodePositionAttribute(value), Format.Doc(
+                "`X Y` (point)",
+                UseKind.Object,
+                "pos=100 0",
+                "position=20 20"
+            ));
 
             // modifies the tracks in which (or the anchors on which) the node is placed
-            yield return Definition.Create("margin", Value.Border, value => new AST.NodeMarginAttribute(value));
+            yield return Definition.Create("margin", Value.Border, value => new AST.NodeMarginAttribute(value), Format.Doc(
+                "`XY`/`X Y`/`X1 Y1 X2 Y2` (border)",
+                UseKind.Object
+            ));
 
             // modifies placement *within* grid tracks (or beside anchors) - the counterpart of "justify" from RegionAttributes
-            yield return Definition.Create("align-horizontal", Attribute.AlignColumn.OrNone(), value => new AST.NodeAlignColumnAttribute(value));
-            yield return Definition.Create("align-vertical", Attribute.AlignRow.OrNone(), value => new AST.NodeAlignRowAttribute(value));
-            yield return Definition.Create("align", Alignment, values => new AST.NodeAlignAttribute(values.horizontal, values.vertical));
+            yield return Definition.Create("align-horizontal", Attribute.AlignColumn.OrNone(), value => new AST.NodeAlignColumnAttribute(value), Format.Doc(
+                "`stretch`, `start`/`left`, `center`, `end`/`right` or `none`",
+                UseKind.Object
+            ));
+
+            yield return Definition.Create("align-vertical", Attribute.AlignRow.OrNone(), value => new AST.NodeAlignRowAttribute(value), Format.Doc(
+                "`stretch`, `start`/`top`, `center`, `end`/`bottom` or `none`",
+                UseKind.Object
+            ));
+
+            yield return Definition.Create("align", Alignment, values => new AST.NodeAlignAttribute(values.horizontal, values.vertical), Format.Doc(
+                "Shorthand for `align-horizontal` and `align-vertical alignment`.",
+                "both values or a single shared value (`stretch`, `start`, `center`, `end` or `none`)",
+                UseKind.Object,
+                "align=center",
+                "align=top right",
+                "align=none end"
+            ));
         }
     }
 }
